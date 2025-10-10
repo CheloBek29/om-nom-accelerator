839d03cbe87d0f92efc69927b99642fd4041bc62
CheloBek29 <77875287+CheloBek29@users.noreply.github.com>
2025-10-10 20:26:25 +0300

    Added scene changing by clicking on shelf at the main room scene

---
+++ Assets/Scripts/MainRoom/ClickManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    [Tooltip("Для того, чтобы настроить объекты, ведущие на другие сцены, добавьте по элементу в списки Choose Scene Game Objects и Choose Scene Names, объект, по нажатию которого перемещать - в первый, а название сцены, куда перемещать - во второй.")]
+    [SerializeField] private List<GameObject> m_chooseSceneGameObjects;
+    [SerializeField] private List<string> m_chooseSceneNames;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitObject = hit.collider.gameObject.transform.parent.gameObject;
+                if (m_chooseSceneGameObjects.Contains(hitObject))
+                {
+                    int sceneId = m_chooseSceneGameObjects.IndexOf(hitObject);
+                    gameObject.GetComponent<SceneChanger>().ChangeScene(m_chooseSceneNames[sceneId]);
+                }
+            }
+        }
+    }
+}
