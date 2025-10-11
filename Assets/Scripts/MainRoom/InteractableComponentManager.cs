0f823a82c46cd6425150f78be490c14f59cac6af
CheloBek29 <77875287+CheloBek29@users.noreply.github.com>
2025-10-11 16:53:06 +0300

    Quality of Life changes at MainRoom scene
    (+Code Refactoring)

---
+++ Assets/Scripts/MainRoom/InteractableComponentManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableComponentManager : MonoBehaviour
+{
+    [Header("Scene Changer")]
+    [SerializeField] private SceneChanger m_sceneChanger;
+
+    [Header("Cursor Settings")]
+    [SerializeField] private Image m_cursor;
+    [SerializeField] private Vector2 m_originalSize;
+    [SerializeField] private Vector2 m_hoverSize;
+
+    [Header("Scene")]
+    [SerializeField] private string m_scene;
+
+    private void OnMouseEnter()
+    {
+        m_cursor.rectTransform.sizeDelta = m_hoverSize;
+        gameObject.GetComponent<Outline>().enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        m_cursor.rectTransform.sizeDelta = m_originalSize;
+        gameObject.GetComponent<Outline>().enabled = false;
+    }
+
+    private void OnMouseDown()
+    {
+        m_sceneChanger.ChangeScene(m_scene);
+    }
+}
