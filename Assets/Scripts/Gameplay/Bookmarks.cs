6e4b6d5d8c633f2921993b445cacfb73e0e3da73
raining-cat <astudyname@gmail.com>
2025-10-11 20:02:30 +0300

    Notebook - Groups

--- Assets/Scripts/Gameplay/Bookmarks.cs
+++ Assets/Scripts/Gameplay/Bookmarks.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Bookmarks : MonoBehaviour
 {
     [SerializeField] private GameObject currentTab;
+    [SerializeField] private GameObject currentGroup;
+
 
     public void ChangeTab(GameObject tab)
     {
@@ -11,5 +13,14 @@ public class Bookmarks : MonoBehaviour
         tab.SetActive(true);
 
         currentTab = tab;
+    }
+
+    public void ChangeGroup(GameObject group)
+    {
+        currentGroup.SetActive(false);
+
+        group.SetActive(true);
+
+        currentGroup = group;
     }
 }
