7a1b2657b3bbbeb039ad413145c6e2101ea9ad06
CheloBek29 <77875287+CheloBek29@users.noreply.github.com>
2025-11-20 23:07:05 +0300

    new NB manager

---
+++ Assets/Scripts/newUI/NbManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class NbManager : MonoBehaviour
+{
+    [SerializeField] private GameObject m_bookmarkHidden, m_bookmarkVisible, m_backToTab;
+    [SerializeField] private GameObject m_currentNote, m_currentTab;
+
+    public void OpenNote(GameObject note)
+    {
+        m_currentTab.SetActive(false);
+        
+        m_currentNote = note;
+        note.SetActive(true);
+
+        m_bookmarkVisible.SetActive(false); m_bookmarkHidden.SetActive(true);
+        m_backToTab.SetActive(true);
+    }
+
+    public void CloseNote()
+    {
+        m_currentNote.SetActive(false);
+        m_currentTab.SetActive(true);
+
+        m_bookmarkVisible.SetActive(true); m_bookmarkHidden.SetActive(false);
+        m_backToTab.SetActive(false);
+    }
+
+    public void ChangeTab(GameObject tab)
+    {
+        m_currentTab.SetActive(false);
+
+        m_currentTab = tab;
+
+        tab.SetActive(true);
+    }
+}
